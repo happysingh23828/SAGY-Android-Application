b44b78294d377aa662afb68c859a51aa73188ddc
mayank <mayankkasera98@gmail.com>
2018-02-02 04:48:50 +0530

    first commit by mayank

--- app/src/main/java/dynamicdrillers/sagy/SearchVillagesActivity.java
+++ app/src/main/java/dynamicdrillers/sagy/SearchVillagesActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class SearchVillagesActivity extends AppCompatActivity {
 
+    private android.support.v7.widget.Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_villages);
+
+        toolbar = findViewById(R.id.simple_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Search Villages");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
