1b92c0ea2c587adc5ce04a04c3548fd7825b267c
Happy Singh <happysingh23828@gmail.com>
2018-02-01 23:13:05 +0530

    Added All The Activities Of App By Happy

--- app/src/main/java/dynamicdrillers/sagy/SearchVillagesActivity.java
+++ app/src/main/java/dynamicdrillers/sagy/SearchVillagesActivity.java
@@ -3,17 +3,11 @@ package dynamicdrillers.sagy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.database.FirebaseDatabase;
-
-public class MainActivity extends AppCompatActivity {
+public class SearchVillagesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.activity_search_villages);
     }
 }
