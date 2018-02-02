ff476195c1df816cc8115d96aad0a8932275cebf
Happy Singh <happysingh23828@gmail.com>
2018-02-02 18:25:40 +0530

    Merging With Happy
    # Conflicts:
    #	app/src/main/java/dynamicdrillers/sagy/MainActivity.java

--- app/src/main/java/dynamicdrillers/sagy/MainActivity.java
+++ app/src/main/java/dynamicdrillers/sagy/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_village_inforamtion);
+        setContentView(R.layout.activity_main);
 
 
     }
