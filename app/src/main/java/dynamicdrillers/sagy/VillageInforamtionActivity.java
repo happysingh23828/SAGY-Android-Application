05766bc573cd7cfc7893c72bb33e15c58371d43b
Happy Singh <happysingh23828@gmail.com>
2018-02-02 13:35:59 +0530

    Merging With Happy
    # Conflicts:
    #	app/src/main/java/dynamicdrillers/sagy/MainActivity.java

--- app/src/main/java/dynamicdrillers/sagy/VillageInforamtionActivity.java
+++ app/src/main/java/dynamicdrillers/sagy/VillageInforamtionActivity.java
@@ -2,12 +2,19 @@ package dynamicdrillers.sagy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class VillageInforamtionActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_village_inforamtion);
+
+        toolbar = (Toolbar)findViewById(R.id.simple_tool_bar);
+        getSupportActionBar().setTitle("Hagshag");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
