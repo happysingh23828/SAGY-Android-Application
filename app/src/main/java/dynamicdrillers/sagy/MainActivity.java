3eb2d000562d81e764b872d0708f128a5841dfe8
mayankkasera <mayankkasera98@gmail.com>
2018-04-10 20:11:48 +0530

    commit

--- app/src/main/java/dynamicdrillers/sagy/MainActivity.java
+++ app/src/main/java/dynamicdrillers/sagy/MainActivity.java
@@ -1,9 +1,12 @@
 package dynamicdrillers.sagy;
 
+import android.app.ActionBar;
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -12,17 +15,22 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
@@ -32,10 +40,13 @@ public class MainActivity extends AppCompatActivity{
     private NavigationView navigationView;
     private TextView searchtexview;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         searchtexview = findViewById(R.id.search_textview);
         searchtexview.setOnClickListener(new View.OnClickListener() {
@@ -65,23 +76,31 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-
+                    Fragment fragment =null;
                 switch (item.getItemId()) {
                     case R.id.Dashboard_item:
+                        viewPager.setCurrentItem(0);
+                        drawerLayout.closeDrawer(Gravity.START);
                         Toast.makeText(getBaseContext(), "DashBoard Clicked", Toast.LENGTH_SHORT).show();
 
 
                         break;
 
                     case R.id.Notifications_item:
+                        viewPager.setCurrentItem(2);
+                        drawerLayout.closeDrawer(Gravity.START);
                         Toast.makeText(getBaseContext(), "Notification Clicked", Toast.LENGTH_SHORT).show();
                         break;
 
                     case R.id.Complaints_item:
-                        Toast.makeText(getBaseContext(), "Complaint Clicked", Toast.LENGTH_SHORT).show();
+                        viewPager.setCurrentItem(1);
+                        drawerLayout.closeDrawer(Gravity.START);
                         break;
 
                     case R.id.Recent_ACtivity_item:
+
+                        viewPager.setCurrentItem(3);
+                        drawerLayout.closeDrawer(Gravity.START);
                         Toast.makeText(getBaseContext(), "Recent Activity  Clicked", Toast.LENGTH_SHORT).show();
                         break;
 
@@ -122,6 +141,7 @@ public class MainActivity extends AppCompatActivity{
 
         tabLayout = findViewById(R.id.main_tablayout);
         tabLayout.setupWithViewPager(viewPager);
+
 
         button = findViewById(R.id.filter_btn);
         button.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/dynamicdrillers/sagy/VIdeosActivity.java
+++ app/src/main/java/dynamicdrillers/sagy/VIdeosActivity.java
@@ -1,13 +1,102 @@
 package dynamicdrillers.sagy;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class VIdeosActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.*;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class VIdeosActivity extends YouTubeBaseActivity{
+    RecyclerView videorv = null;
+    DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
+        videorv = findViewById(R.id.videosRv);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        videorv.setLayoutManager(linearLayoutManager);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("videos");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Query query = mDatabase;
+
+        FirebaseRecyclerOptions<ModelVideos> options = new FirebaseRecyclerOptions.Builder<ModelVideos>()
+                .setQuery(query,ModelVideos.class)
+                .build();
+        FirebaseRecyclerAdapter<ModelVideos,ModelVideosViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<ModelVideos, ModelVideosViewHolder>
+                (options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ModelVideosViewHolder holder, int position, @NonNull ModelVideos model) {
+                    holder.setTitle(model.getTitle());
+                    holder.setYoutubeid(model.getYoutubeid());
+            }
+
+            @Override
+            public ModelVideosViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view =  LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.videos_item, parent, false);
+                return new ModelVideosViewHolder(view);
+            }
+        };
+        videorv.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+
+    }
+
+    public static class ModelVideosViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+        public ModelVideosViewHolder(View itemView) {
+            super(itemView);
+            this.mView = itemView;
+        }
+
+        public void setTitle(String title) {
+            Button mbutton = mView.findViewById(R.id.videotitle);
+            mbutton.setText(title);
+        }
+
+        public void setYoutubeid(final String youtubeid) {
+
+        YouTubePlayerView youTubePlayerView = mView.findViewById(R.id.youtube);
+
+        youTubePlayerView.initialize(YouTubeConfig.API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo(youtubeid);
+                youTubePlayer.pause();
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+
+
+        }
+    }
+
+
 }
